beef41bad06c27dbaff3fd4b572da04e9a0559f4
Sai Petluri <spetluri.1@gmail.com>
2021-02-27 02:20:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:nil,name:,type:,db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  
+  
 end
